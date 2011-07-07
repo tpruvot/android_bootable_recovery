171c3d7d03106aab8dc43d8a8c5fb469e3a5c4d5
Koushik Dutta <koushd@gmail.com>
2011-07-07 12:44:05 -0700

    backup and restore of /sdcard/Android (see getExternalFilesDir)
    Change-Id: I6306464cdce4e3b48e0d109284e5606f65a84ee2
    
    Conflicts:
    
    	nandroid.c

--- recovery.c
+++ recovery.c
@@ -692,6 +692,7 @@ wipe_data(int confirm) {
     }
     erase_volume("/sd-ext");
     erase_volume("/sdcard/.android_secure");
+    erase_volume("/sdcard/Android");
     ui_print("Data wipe complete.\n");
 }
 
@@ -841,7 +842,7 @@ main(int argc, char **argv) {
         case 'p': previous_runs = atoi(optarg); break;
         case 's': send_intent = optarg; break;
         case 'u': update_package = optarg; break;
-        case 'w': 
+        case 'w':
 #ifndef BOARD_RECOVERY_ALWAYS_WIPES
 		wipe_data = wipe_cache = 1;
 #endif
