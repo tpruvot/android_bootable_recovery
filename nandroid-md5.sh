b988fef6494a6ec803718af2a52fd0c4924ba826
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-09-23 05:53:57 +0200

    dont do md5sum on md5 files

--- nandroid-md5.sh
+++ nandroid-md5.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 cd $1
 rm -f /tmp/nandroid.md5
-md5sum * .* > /tmp/nandroid.md5
+md5sum * .* | grep -v "md5" > /tmp/nandroid.md5
 cp /tmp/nandroid.md5 .
 # need this because wildcard seems to cause md5sum to return 1
 if [ -f nandroid.md5 ]
