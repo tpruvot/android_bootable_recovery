1fb3cdecda6a20413ade8aeb607248cd8afdcf5f
Danesh Mondegarian <daneshm90@gmail.com>
2011-10-11 16:52:23 -0400

    Recovery : Show menu if in non user initiated mode and error occurred
    When using rom manager and applying a zip, if one encounters an error in recovery,
    they're left on a screen with no visible controls (Forcing to hard reset phone).
    
    Change-Id: Ia9b2f396fc95c7972a5a5580e1a50adf4af26060

--- recovery.c
+++ recovery.c
@@ -982,7 +982,10 @@ main(int argc, char **argv) {
         }
     }
 
-    if (status != INSTALL_SUCCESS && !is_user_initiated_recovery) ui_set_background(BACKGROUND_ICON_ERROR);
+    if (status != INSTALL_SUCCESS && !is_user_initiated_recovery) {
+        ui_set_show_text(1);
+        ui_set_background(BACKGROUND_ICON_ERROR);
+    }
     if (status != INSTALL_SUCCESS || ui_text_visible()) {
         if (!ui_text_visible()) {
             ui_set_show_text(1);
