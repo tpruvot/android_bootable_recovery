977a7cf884a420a0a6adc52155a0243b24eb0af7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-11-27 13:11:15 +0100

    allow to build for x86 targets

--- flashutils/Android.mk
+++ flashutils/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(TARGET_SIMULATOR),true)
-ifeq ($(TARGET_ARCH),arm)
-
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := flashutils.c
 LOCAL_MODULE := libflashutils
@@ -102,5 +99,3 @@ LOCAL_STATIC_LIBRARIES := libflashutils libmtdutils libmmcutils libbmlutils libc
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
-endif	# TARGET_ARCH == arm
-endif	# !TARGET_SIMULATOR
