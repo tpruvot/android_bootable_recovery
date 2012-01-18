b5f90b73587bb5ba8ad81a26df04796c00787f46
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-18 23:46:23 +0100

    use same colors in bootmenu and recovery
    Change-Id: I5c53980656ccf385be7b6989e925e7120727f5e0

--- ui.c
+++ ui.c
@@ -79,7 +79,7 @@ static int ui_log_stdout = 1;
 static const struct { gr_surface* surface; const char *name; } BITMAPS[] = {
     { &gBackgroundIcon[BACKGROUND_ICON_INSTALLING], "icon_installing" },
     { &gBackgroundIcon[BACKGROUND_ICON_ERROR],      "icon_error" },
-    { &gBackgroundIcon[BACKGROUND_ICON_CLOCKWORK],  "icon_clockwork" },
+    { &gBackgroundIcon[BACKGROUND_ICON_CLOCKWORK],  "icon_bootmenu" },
     { &gBackgroundIcon[BACKGROUND_ICON_FIRMWARE_INSTALLING], "icon_firmware_install" },
     { &gBackgroundIcon[BACKGROUND_ICON_FIRMWARE_ERROR], "icon_firmware_error" },
     { &gProgressBarIndeterminate[0],    "indeterminate1" },
@@ -189,8 +189,7 @@ static void draw_text_line(int row, const char* t) {
 //#define MENU_TEXT_COLOR 0, 191, 255, 255  //cyan   v5
 //#define MENU_TEXT_COLOR 255, 1, 1, 200    //red    dev
 //#define MENU_TEXT_COLOR 7, 255, 1, 255    //lime
-#define MENU_TEXT_COLOR 255, 200, 1, 1      //blue   ics
-
+#define MENU_TEXT_COLOR 64, 96, 255, 255 //blue
 #define NORMAL_TEXT_COLOR 200, 200, 200, 255
 #define HEADER_TEXT_COLOR NORMAL_TEXT_COLOR
 
