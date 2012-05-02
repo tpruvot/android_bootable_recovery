978bd22488a306b678c1ead63a63ca368fe27418
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-05-02 10:36:47 +0200

    use repo e2fsprogs: tune2fs, mke2fs and e2fsck
    fix overrides warnings at the start of build
    
    Change-Id: Id3067d4da53643ae1af0b9f15445262afda7b197

--- mmcutils/Android.mk
+++ mmcutils/Android.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(BOARD_HAS_LARGE_FILESYSTEM),true)
-LOCAL_CFLAGS += -DBOARD_HAS_LARGE_FILESYSTEM
-endif
-
 LOCAL_SRC_FILES := \
 	mmcutils.c
 
